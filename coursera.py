77f4443db45c20af02e98712f980a28adc7e87cc
igorzakhar <igorzakhar@gmail.com>
2017-05-21 10:20:56 +0300

    Rename function and variables

--- coursera.py
+++ coursera.py
@@ -14,18 +14,36 @@ CourseInfo = namedtuple('CourseInfo', ('course_name', 'start_date',
                                        'user_rating'))
 
 
-async def get_courses_list(session):
+async def get_response_data(url, session):
+    async with session.get(url) as response:
+        return await response.read()
+
+
+async def get_courses_urls(session):
     url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    response = await get_response(url, session)
+    response = await get_response_data(url, session)
     response = response.decode('utf-8')
-    #response = requests.get(url)
     root = etree.fromstring(response)
     url_list = [child[0].text for child in root]
-    random_urls = random.sample(url_list, 10)
+    random_urls = random.sample(url_list, 20)
     return random_urls
 
 
-def get_info(page):
+async def get_course_pages():
+    results = []
+    async with ClientSession() as session:
+        random_links = await get_courses_urls(session)
+        tasks=[asyncio.ensure_future(
+            get_response_data(url, session)) for url in random_links]
+        to_do_iter = asyncio.as_completed(tasks)
+        progress_meter_wrap = tqdm.tqdm(to_do_iter, total=len(random_links))
+        for future in progress_meter_wrap:
+            res = await future
+            results.append(res.decode('utf-8'))
+    return results
+
+
+def parse_course_info(page):
     length_course = None
     user_rating = None
     soup = BeautifulSoup(page, 'lxml')
@@ -50,30 +68,15 @@ def get_info(page):
                              language=language, length_course=length_course,
                              user_rating=user_rating)
     return course_info
-    
-    return course_name
 
 
-async def get_response(url, session):
-    async with session.get(url) as response:
-        return await response.read()
-        
+def get_courses_info(pages):
+    courses_info = []
+    for page in pages:
+        course_info = parse_course_info(page)
+        courses_info.append(course_info)
+    return courses_info
 
-async def get_contents():
-    results = []
-    async with ClientSession() as session:
-        random_links = await get_courses_list(session)
-        print('Links received')
-        tasks=[asyncio.ensure_future(
-            get_response(url, session)) for url in random_links]
-        to_do_iter = asyncio.as_completed(tasks)
-        to_do_iter = tqdm.tqdm(to_do_iter, total=len(random_links))
-        for future in to_do_iter:
-            res = await future
-            results.append(res.decode('utf-8'))
-        #await asyncio.wait(tasks)
-    return results
-    
 
 def output_courses_info_to_xlsx(courses_info_list, filepath):
     workbook = Workbook()
@@ -87,22 +90,12 @@ def output_courses_info_to_xlsx(courses_info_list, filepath):
                           course.length_course,
                           course.user_rating])
     workbook.save(filepath)
-
-
-def get_courses_info(pages):
-    courses_info = []
-    for page in pages:
-        course_info = get_info(page)
-        courses_info.append(course_info)
-    return courses_info
+    print('File was saved')
 
 
 if __name__ == '__main__':
-    #s = time.time()
-    #links = get_courses_list()
-    #print('Links received')
+
     loop=asyncio.get_event_loop()
-    res = loop.run_until_complete(get_contents())
-    courses_info_list = get_courses_info(res)
+    courses_page = loop.run_until_complete(get_course_pages())
+    courses_info_list = get_courses_info(courses_page)
     output_courses_info_to_xlsx(courses_info_list, 'sample.xlsx')
-    print(courses_info_list)
