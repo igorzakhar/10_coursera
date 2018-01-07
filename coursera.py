79ac9a6f4e928a3dea6ac7eeb5a66b42fa50497f
igorzakhar <igorzakhar@gmail.com>
2018-01-07 13:56:14 +0300

    Refactored almost all code

--- coursera.py
+++ coursera.py
@@ -1,107 +1,118 @@
 import asyncio
 import random
-from collections import namedtuple
+import time
 import xml.etree.ElementTree as etree
 
-import tqdm
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
-from aiohttp import ClientSession
 
 
-CourseInfo = namedtuple('CourseInfo', ('course_name', 'start_date',
-                                       'language', 'length_course',
-                                       'user_rating'))
-
-
-async def get_response_data(url, session):
+async def fetch_response(url, session):
     async with session.get(url) as response:
-        return await response.read()
+        return await response.text(encoding='utf-8')
 
 
-async def get_courses_urls(session):
-    url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    response = await get_response_data(url, session)
-    response = response.decode('utf-8')
-    root = etree.fromstring(response)
+async def fetch_xml(session):
+    xml = 'https://www.coursera.org/sitemap~www~courses.xml'
+    body = await fetch_response(xml, session)
+    return body
+
+
+def get_random_urls(body):
+    number_links = 20
+    root = etree.fromstring(body)
     url_list = [child[0].text for child in root]
-    random_urls = random.sample(url_list, 20)
+    random_urls = random.sample(url_list, number_links)
     return random_urls
 
 
-async def get_course_pages(loop):
-    results = []
-    async with ClientSession(loop=loop) as session:
-        random_links = await get_courses_urls(session)
-        tasks=[asyncio.ensure_future(
-            get_response_data(url, session)) for url in random_links]
-        to_do_iter = asyncio.as_completed(tasks)
-        progress_meter_wrap = tqdm.tqdm(to_do_iter, total=len(random_links))
-        for future in progress_meter_wrap:
-            res = await future
-            results.append(res.decode('utf-8'))
-    return results
+async def fetch_courses_html():
+    async with ClientSession() as session:
+        body = await fetch_xml(session)
+        urls = get_random_urls(body)
+        tasks = [
+            asyncio.ensure_future(fetch_response(url, session))
+            for url in urls
+        ]
+        courses_html = await asyncio.gather(*tasks)
+    return courses_html
 
 
-def parse_course_info(page):
-    length_course = None
-    user_rating = None
-    soup = BeautifulSoup(page, 'lxml')
+def parse_course_info(html):
+    soup = BeautifulSoup(html, 'lxml')
     course_name = soup.find('h1', {'class': 'title'}).text
     start_date = soup.find('div', {'class': 'startdate'}).text
-    language = soup.find('div', {'class':'rc-Language'}).text
+    language = soup.find('div', {'class': 'rc-Language'}).text
 
-    if soup.find('div', {'class':'rc-WeekView'}):
-        length_course = soup.find('div', {'class':'rc-WeekView'})
+    if soup.find('div', {'class': 'rc-WeekView'}):
+        length_course = soup.find('div', {'class': 'rc-WeekView'})
         length_course = str(len(length_course)) + ' week(s)'
-    elif soup.find('i', {'class':'cif-clock'}):
-        length_course = soup.find('i', {'class':'cif-clock'}).parent.next_sibling.text
+    elif soup.find('i', {'class': 'cif-clock'}):
+        length_course = soup.find('i', {'class': 'cif-clock'})
+        length_course = length_course.parent.next_sibling.text
     else:
-        length_course = 'No data'
+        length_course = None
 
     if soup.find('div', {'class': 'ratings-text'}):
         user_rating = soup.find('div', {'class': 'ratings-text'}).text
     else:
-        user_rating = 'No data'
+        user_rating = None
 
-    course_info = CourseInfo(course_name=course_name, start_date=start_date,
-                             language=language, length_course=length_course,
-                             user_rating=user_rating)
-    return course_info
-
-
-def get_courses_info(pages):
-    courses_info = []
-    for page in pages:
-        course_info = parse_course_info(page)
-        courses_info.append(course_info)
-    return courses_info
+    return {
+        'course_name': course_name,
+        'start_date': start_date,
+        'language': language,
+        'length_course': length_course,
+        'user_rating': user_rating
+    }
 
 
-def output_courses_info_to_xlsx(courses_info_list, filepath):
+def fill_xlsx(courses_info):
     workbook = Workbook()
     worksheet = workbook.active
-    worksheet.append(['Course name', 'Start date', 
-                      'Language', 'Length of the course', 'User rating'])
-    for course in courses_info_list:
-        worksheet.append([course.course_name,
-                          course.start_date,
-                          course.language,
-                          course.length_course,
-                          course.user_rating])
-    
+    worksheet.append([
+        'Course name',
+        'Start date',
+        'Language',
+        'Length of the course',
+        'User rating'
+    ])
+
+    for course in courses_info:
+        if not course['user_rating']:
+            course['user_rating'] = 'Not yet rating'
+        worksheet.append([
+            course['course_name'],
+            course['start_date'],
+            course['language'],
+            course['length_course'],
+            course['user_rating']
+        ])
+
     for column_cells in worksheet.columns:
         length = (max(len(str(cell.value)) for cell in column_cells))
         worksheet.column_dimensions[column_cells[0].column].width = length
-        
-    workbook.save(filepath)
-    print('File \'{}\' has been saved'.format(filepath))
+
+    return workbook
+
+
+def save_workbook(workbook, filename):
+    try:
+        workbook.save(filename)
+    except OSError as error:
+        return error
 
 
 if __name__ == '__main__':
-
-    loop=asyncio.get_event_loop()
-    courses_page = loop.run_until_complete(get_course_pages(loop))
-    courses_info_list = get_courses_info(courses_page)
-    output_courses_info_to_xlsx(courses_info_list, 'couesera.xlsx')
+    loop = asyncio.get_event_loop()
+    courses_html = loop.run_until_complete(fetch_courses_html())
+    courses_info = [parse_course_info(html) for html in courses_html]
+    workbook = fill_xlsx(courses_info)
+    filepath = 'couesera.xlsx'
+    error = save_workbook(workbook, filepath)
+    if not error:
+        print('File \'{}\' has been saved'.format(filepath))
+    else:
+        print(error)
     loop.close()
